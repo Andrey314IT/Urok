f4bd2c6ce95ed3a42a4af7264853c8661cd2d3e5
Andrey314IT <125789209+Andrey314IT@users.noreply.github.com>
2023-03-19 07:39:40 +0000

    Added find_all

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from bs4 import BeautifulSoup
 URL = "https://coinmarketcap.com/"
 
 res = requests.get(URL)
+
+if res.status_code == 200:
+  soup = BeautifulSoup(res.text, feature="html.parser")
+  info = soup.find_all("a", {"href":"/currencies/bitcoin/markets/"})
+  print(info[0])
